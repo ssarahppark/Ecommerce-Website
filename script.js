0031b2ddff00ef4b86b82b13247b413d54e4a70a
Sarah <115687454+ssarahppark@users.noreply.github.com>
2024-06-02 17:36:00 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+const addToCartButton = document.getElementById('add-to-cart-button');
+
+if (addToCartButton) {
+    addToCartButton.addEventListener('click', function() {
+        addToCartButton.classList.add('added');
+        setTimeout(function(){
+            addToCartButton.classList.remove('added');
+            window.location.href = 'cart.html'; // Redirect to cart.html
+        }, 2000);
+    });
+}
